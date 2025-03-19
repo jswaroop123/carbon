deb97e51ba05bc8a1ccceb073b49b7a070a67b0a
Jyothi Swaroop N <jswaroop2003@gmail.com>
2025-03-19 12:06:38 +0530

    add endpoint to retrieve all students from the database

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
-import { Prisma } from '@prisma/client';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient()
 const app = new Hono()
+
+app.get('/student', async (c) => {
+  const student = await prisma.student.findMany()
+  return c.json({student},200)
+})
+
+
+
 serve(app);
 console.log(`Server is running on http://localhost:${3000}`)
