0a5afc2f481909c261391bd9998df4d04f254fa6
Jyothi Swaroop N <jswaroop2003@gmail.com>
2025-03-19 14:41:34 +0530

    remove uniqueness constraint on proctorId in Student model and update migration file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient()
-const app = new Hono()
 
-app.get("/student", async (context) => {  
-  try{
+const hono = new Hono()
+
+
+hono.get("/student", async (context) => {  
   const student = await prisma.student.findMany();
 
   return context.json(
@@ -14,15 +16,14 @@ app.get("/student", async (context) => {
       student,
     },
     200
-  );}
-  catch (error) {
-    console.error(error);
-  }
+  );
 })
 
-app.post('/student', async (c) => {
-  try{
-  const { name, dateOfBirth, aadharNumber, proctorId } = await c.req.json()
+
+
+hono.post("/student", async (context) => {
+  const { name, dateOfBirth, aadharNumber, proctorId } = await context.req.json();
+try{
   const student = await prisma.student.create({
     data: {
       name,
@@ -30,15 +31,59 @@ app.post('/student', async (c) => {
       aadharNumber,
       proctorId,
     },
-  })
-  return c.json(student)}
-  catch (error) {
-    console.error(error);
-  }
+  });
+
+  return context.json(
+    {
+      student,
+    },
+    200
+  );
+}
+
+catch(error){
+  console.error("Error fetching students:", error);
+}
 })
 
 
 
+hono.post("/professor", async (context) => {
+  const{id,name,seniority,aadharNumber} = await context.req.json();
+  try{
+  const proctor = await prisma.professor.create({
+    data: {
+      id,
+      name,
+      seniority,
+      aadharNumber,
+    },
+  });
+  
+  return context.json(
+    {
+      proctor,
+    },
+    200
+  );
+}
+catch(error){
+  console.error("Error:", error);
+}
+});
 
-serve(app);
+hono.get("/professor", async (context) => {
+  const proctor = await prisma.professor.findMany();
+
+  return context.json(
+    {
+      proctor,
+    },
+    200
+  );
+})
+
+
+
+serve(hono);
 console.log(`Server is running on http://localhost:${3000}`)
