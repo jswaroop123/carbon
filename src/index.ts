9cc8045341ffffb232a1c21266f55c94869a588c
Jyothi Swaroop N <jswaroop2003@gmail.com>
2025-03-19 12:19:35 +0530

    add endpoint to create a new student

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,38 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient()
 const app = new Hono()
 
-app.get('/student', async (c) => {
-  const student = await prisma.student.findMany()
-  return c.json({student},200)
+app.get("/student", async (context) => {  
+  try{
+  const student = await prisma.student.findMany();
+
+  return context.json(
+    {
+      student,
+    },
+    200
+  );}
+  catch (error) {
+    console.error(error);
+  }
 })
+
+app.post('/student', async (c) => {
+  try{
+  const { name, dateOfBirth, aadharNumber, proctorId } = await c.req.json()
+  const student = await prisma.student.create({
+    data: {
+      name,
+      dateOfBirth,
+      aadharNumber,
+      proctorId,
+    },
+  })
+  return c.json(student)}
+  catch (error) {
+    console.error(error);
+  }
+})
+
 
 
 
